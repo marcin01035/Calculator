3f57b4e8f93f2f355eefcdacff46c6c2fc7d03d0
marcin01035 <marcinmiskowiec.ns@gmail.com>
2018-12-16 16:22:13 +0100

    atualizacja serwera

---
+++ client.py
@@ -0,0 +1,49 @@
+import Pyro4
+
+
+uri = input("Jakie jest Pyro uri? ").strip()
+
+date_receiver = Pyro4.Proxy(uri)	#podlaczenie do servera z uzyciem uri
+
+
+
+def main():
+    print("Podaj dwie liczby, na których wybrana operacja zostanie wykonana")
+    x = int(input())
+    y = int(input())
+    print("Wybierz operacje, która chcesz wykonac na dwóch liczbach")
+    print(" 1- dodawanie\n 2- odejmowanie\n 3- iloczyn\n 4- iloraz")
+    z = int(input())
+    if z>=1 and z<5:
+
+        if z==1:
+            return print("Oto wynik dzialania:",date_receiver.sum(x, y))
+        if z==2:
+            return print("Oto wynik dzialania:",date_receiver.difference(x, y))
+        if z==3:
+            return print("Oto wynik dzialania:",date_receiver.product(x, y))
+        if z==4:
+            return print("Oto wynik dzialania:",date_receiver.quotient(x, y))
+    else:
+        print("Wybór jest niepoprawny! Spobuj jeszcze raz")
+        return False
+
+
+
+while True:
+    main()
+    print("Czy chcesz dokonaæ nastepnej operacji? Prosze wybrac y/n")
+    if input() == "y":
+        main()
+    else:
+        print("Dziekujmy za skorzystanie z uslug naszego serwisu :)")
+        break
+
+
+
+
+
+
+
+
+
---
+++ server.py
@@ -0,0 +1,28 @@
+import Pyro4
+
+@Pyro4.expose
+
+class calcul(object):
+	def sum(self, x, y):
+		result = x+y
+		return result
+
+	def difference(self, x, y):
+		result = x - y
+		return result
+
+	def product(self, x, y):
+		result = x*y
+		return result
+
+	def quotient(self, x, y):
+		result = x/y
+		return result
+############################################################################
+#powolanie servera
+daemon = Pyro4.Daemon()  			#tworzenie Deamona dla Pyro
+uri = daemon.register(calcul)			#binding MyClass jako obiet Pyro
+
+print("Server ruszyl.  Objekt uri= ", uri)		#wypisanie adresu uri
+daemon.requestLoop()				#tryb nasluchiwania deamona
+
